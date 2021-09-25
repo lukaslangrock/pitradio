23e2bfead34e33cdedf0fdb3dfff592218e4edf4
Noaaaaaah <noah.gertzen@gmail.com>
2021-09-25 19:17:10 +0200

    Add Button Project

---
+++ buttons/ButtonCSControl/ButtonCSControl/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using RaspberryPiDotNet;
+
+namespace Gpio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // GPIO's initialisieren
+            GPIOMem mDown = new GPIOMem(GPIOPins.V2_GPIO_8);
+            GPIOMem next = new GPIOMem(GPIOPins.V2_GPIO_10);
+            GPIOMem play = new GPIOMem(GPIOPins.V2_GPIO_12);
+            GPIOMem back = new GPIOMem(GPIOPins.V2_GPIO_16);
+
+            while (true)
+            {
+                
+                // GPIO lesen
+                if (mDown.Read() == true)
+                {
+                    Console.WriteLine("mDown")
+                }
+                if (next.Read() == true)
+                {
+                    Console.WriteLine("next")
+                }
+                if (play.Read() == true)
+                {
+                    Console.WriteLine("play")
+                }
+                if (back.Read() == true)
+                {
+                    Console.WriteLine("back")
+                }
+            }
+        }
+    }
+}
