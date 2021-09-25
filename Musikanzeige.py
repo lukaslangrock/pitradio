a3e3da888f1a171ce73bc226586a36d1ac40c175
Noaaaaaah <75997595+Noaaaaaah@users.noreply.github.com>
2021-09-25 18:17:07 +0200

    Update Musikanzeige.py

--- Musikanzeige.py
+++ Musikanzeige.py
@@ -1,9 +1,16 @@
 import json
+import codecs
 import time
 import os
-cname = "Name"
-cinterpret = "Interpret"
-clength = 200
+import sys
+import numpy as np
+
+cstate = str(sys.argv[1])
+cname = sys.argv[2]
+cinterpret = sys.argv[3]
+clength = sys.argv[4]
+ctime = sys.argv[5]
+
 
 def updateSong():
     os.system('cls' if os.name == 'nt' else 'clear') 
@@ -12,16 +19,25 @@ def updateSong():
 
 
 def progressBar(current, total, barLength = 25):
-    percent = float(current) * 100 / total
+    percent = float(current) * 100 / int(total)
     arrow   = '-' * int(percent/100 * barLength - 1) + '>'
     spaces  = ' ' * (barLength - len(arrow))
 
     print('Progress: [%s%s] %d %%' % (arrow, spaces, percent), end='\r')
 
-def jsonCheck():
-   f = open('musicdb.json', 'r')
-   db = json.load(f)
+def progressedBar(current, total, barLength = 25):
+    percent = float(current) * 100 / int(total)
+    arrow   = '-' * int(percent/100 * barLength - 1) + '>'
+    spaces  = ' ' * (barLength - len(arrow))
 
-   print(db['teacher_db'][0]['name'])
+    print('Progress: [%s%s] %d  %%' % (arrow, spaces, percent))
 
-jsonCheck()
+
+updateSong()
+if(cstate == "play"):
+    while(int(ctime)<int(clength)):
+        progressBar(ctime,clength)
+        time.sleep(1)
+        ctime =  1 + int(ctime)
+else:
+    progressedBar(ctime,clength)
