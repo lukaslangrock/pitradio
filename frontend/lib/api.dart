b222b75b5757c5747ec019be38f886490bdf58da
Darxoon <darxoon@outlook.com>
2021-09-26 11:40:23 +0200

    Add new API endpoints

--- frontend/lib/api.dart
+++ frontend/lib/api.dart
@@ -3,11 +3,24 @@ import 'dart:convert';
 import 'package:http/http.dart';
 import 'package:pitradio/song.dart';
 
+import 'album.dart';
+
 class API {
   String baseUrl = "/";
   Client client = Client();
 
   API(this.baseUrl);
+
+  Future<List<Album>> getAllAlbums() async {
+    var url = Uri.parse(baseUrl + "/Album/GetAllAlbums");
+    var response = await client.get(url);
+    var list = jsonDecode(response.body) as List<dynamic>;
+    return list.map((j) => Album.fromJson(j)).toList(growable: false);
+  }
+
+  String getAlbumArtworkByAlbumUUID(String albumUUID) {
+    return "$baseUrl/Album/GetAlbumArtworkByAlbumUUID/$albumUUID";
+  }
 
   Future<Song> getCurrentSong() async {
     var url = Uri.parse(baseUrl + "/Playlist/GetCurrentSong");
